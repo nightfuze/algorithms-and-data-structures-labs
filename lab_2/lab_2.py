87ea598237f1a0320dea9d274993f1c327471088
nightfuze <nightfuze@bk.ru>
2024-03-01 00:46:43 +0400

    refactor(lab_2): update transformation logic

--- lab_2/lab_2.py
+++ lab_2/lab_2.py
@@ -1,9 +1,15 @@
-# Написать программу, которая читая символы из бесконечной последовательности
-# (эмулируется конечным файлом, читающимся поблочно), распознает, преобразует и выводит на экран лексемы
-# по определенному правилу. Лексемы разделены пробелами. Преобразование делать по возможности через словарь.
-# Для упрощения под выводом числа прописью подразумевается последовательный вывод всех цифр числа.
-# Используя регулярные выражения.
-# Целые нечетные числа. Замена: первая цифра каждого четного числа на нечетном месте на английскую цифру прописью.
+"""
+Написать программу, которая, распознает, преобразует и выводит на экран лексемы по определенному правилу.
+Лексемы разделены пробелами. Преобразование делать по возможности через словарь.
+Для упрощения под выводом числа прописью подразумевается последовательный вывод всех цифр числа.
+
+Входной файл является обыкновенным (т.е. нет требования на «бесконечность» файла);
+Распознавание и обработку делать через регулярные выражения;
+В вариантах, где есть параметр (например К), допускается его заменить на любое число;
+Все остальные требования соответствуют варианту задания лабораторной работы №1.
+
+Целые нечетные числа. Замена: первая цифра каждого четного числа на нечетном месте на английскую цифру прописью.
+"""
 import re
 from typing import List, Dict
 
@@ -27,36 +33,28 @@ def replace_first_digit(number: int) -> str:
 
 
 def extract_integers(text: str) -> List[int]:
-    matches = re.findall(r"-?(?<![.\d])\d+(?![.\d])", text)
-    return [int(x) for x in matches]
+    matches: List[str] = re.findall(r"-?(?<![.\d])\d+(?![.\d])", text)
+    return list(map(int, matches))
 
 
-def process_file(file_name: str, buffer_size: int) -> None:
+def process_file(file_name: str) -> None:
     try:
         with open(file_name, 'r') as file:
-            while True:
-                result: List[str] = []
-                buffer: str = file.read(buffer_size)
-                if not buffer:
-                    break
-                lexemes_list: List[str] = buffer.split()
-                for lexeme_item in lexemes_list:
-                    numbers: List[int] = extract_integers(lexeme_item)
-                    for number in numbers:
-                        if number % 2 == 0:
-                            replaced_number: str = replace_first_digit(number)
-                            result.append(replaced_number)
-                        else:
-                            result.append(str(number))
-                print(*result)
+            read_data: str = file.read()
+            lexemes_list: List[str] = re.findall(r'\S+', read_data)
+            integers_list: List[int] = extract_integers(" ".join(lexemes_list))
+            even_integers_list: List[str] = re.findall(r'-?\b\d*[02468]\b', " ".join(list(map(str, integers_list))))
+            odd_integers_list: List[str] = re.findall(r'-?\b\d*[13579]\b', " ".join(list(map(str, integers_list))))
+            replaced_even_integers_list: List[str] = list(map(replace_first_digit, list(map(int, even_integers_list))))
+            transformed_integers: List[str] = replaced_even_integers_list + odd_integers_list
+            print(*transformed_integers)
     except FileNotFoundError:
         print("Файл не найден")
 
 
 def lab_2():
-    buffer_size: int = 32
     file_name: str = "input.txt"
-    process_file(file_name, buffer_size)
+    process_file(file_name)
 
 
 lab_2()
